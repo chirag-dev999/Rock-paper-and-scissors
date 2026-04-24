54f23137ee22142219bcc81d767476f1b621acce
chirag gupta <chirag.97735248@gmail.com>
2026-04-24 12:37:01 +0530

    bug fixes

--- pgm.js
+++ pgm.js
@@ -2,24 +2,33 @@ const Rock = document.getElementById("Rock");
 
 let Cpu_choice = ["Rock", "paper", "Scissors"];
 
-let Cpu_move = Cpu_choice[Math.floor(Math.random() * Cpu_choice.length)];
-
-const card = document.querySelector(".playercard");
+const messageArea = document.querySelector(".message-area");
 
 Rock.addEventListener("click", function () {
+  let Cpu_move =
+    Cpu_choice[Math.floor(Math.random() * Cpu_choice.length)];
+
   if (Cpu_move == "Rock") {
     const paragraph = document.createElement("p");
+
     paragraph.textContent = "It's a tie, both chose rock";
-    card.appendChild(paragraph);
+
+    paragraph.style.opacity = "0";
+
+    paragraph.style.transition = "opacity 0.5s ease-in-out";
+
+    messageArea.appendChild(paragraph);
 
     setTimeout(() => {
-      paragraph.style.position = "relative";
-      paragraph.style.bottom = "100px";
-      paragraph.style.transition = "all 0.5s";
+      paragraph.style.opacity = "1";
+    }, 10);
 
-      setTimeout(() => {
-        paragraph.remove();
-      }, 500);
+    setTimeout(() => {
+      paragraph.style.opacity = "0";
     }, 2000);
+
+    setTimeout(() => {
+      paragraph.remove();
+    }, 2500);
   }
 });
